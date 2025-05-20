62258b112ca56cf0c5553c486850737711af7480
גיל צמח <tsemachg@auth.ad.bgu.ac.il>
2025-05-20 15:28:53 +0300

    Updating OPTRAM and moving code from "test" to "function"

--- code/prepare_optram.R
+++ code/prepare_optram.R
@@ -1,12 +1,11 @@
 # Run OPTRAM---------------------------------------------------------
 # Description: Use rOPTRAM package to prepare soil moisture rasters
-# Input: 
+# Input: Parameter.yaml file contains all necessary information
 # Returns:
-# Requires:
+# Requires: eOPTRAM, CDSE, yaml, sf
 # Written By: Hagar Boneh, Noa Cohen and Gill Tsemach
 
 ## loading packages
-library(terra)
 library(sf)
 library(sfc)
 library(CDSE)
@@ -16,46 +15,63 @@ library(rOPTRAM)
 # optram_options()
 # checked that scm_mask is set to TRUE
 
+##------------------------- Test section --------------------------
+?optram_safe
+
+# optram_options()
+# optram_options("overwrite", TRUE, FALSE)
+# optram_options("plot_colors", TRUE, FALSE)
 
 
-work_dir = getwd()
-params <- read_yaml(file.path(work_dir, "parameters.yml"))
-acquire_scihub(save_creds = TRUE, clientid = params$clientid, 
-               secret = params$secret)
 
-optram_options()
-
-getwd()
-work_dir = getwd()
-
-## read parameters
-params <- read_yaml(file.path(work_dir, "parameters.yml"))
-
-aoi <- sf::read_sf(file.path(work_dir, params$aoi_file))
-from = params$from_date
-to = params$to_date
-Output_dir <- params$Output_dir
-
-
-optram(aoi, from, to, Output_dir, Output_dir)
-print("OPTRAM done")
-
-
-# Prepare_OPTRAM_Model <- function() {
-#   
-#   getwd()
-#   work_dir = getwd()
-#   
-#   ## read parameters
-#   params <- read_yaml(file.path(work_dir, "parameters.yml"))
-#   aoi <- sf::read_sf(file.path(work_dir, params$aoi_file))
-#   
-#   from = params$from_date
-#   to = params$to_date
-#   Output_dir <- params$Output_dir
-#   
+# getwd()
+# work_dir = getwd()
 # 
-#   optram(aoi, from, to, Output_dir, Output_dir)
-#   t0 <- Sys.time()
-#   message(t0, "OPTRAM complete")
-# }
+# ## read parameters
+# params <- read_yaml(file.path(work_dir, "parameters.yml"))
+# 
+# ## this is to set client ID and secret
+# # acquire_scihub(save_creds = TRUE, clientid = params$clientid, secret = params$secret)
+# 
+# aoi <- sf::read_sf(file.path(work_dir, params$aoi_file))
+# from = params$from_date
+# to = params$to_date
+# Output_dir <- params$Output_dir
+# 
+# 
+# optram(aoi, from, to, Output_dir, Output_dir)
+# print("OPTRAM done")
+# 
+# readRDS("Output/VI_STR_data.rds")
+
+
+
+
+## ------------------------ Code section -------------------------
+
+## this is to set client ID and secret
+acquire_scihub(save_creds = TRUE, clientid = params$clientid, secret = params$secret)
+  
+Prepare_OPTRAM_Model <- function() {
+
+  t0 <- Sys.time() # measure run time
+  message(t0, "OPTRAM Initialize")
+  
+  getwd()
+  work_dir = getwd()
+  
+
+  ## read parameters
+  params <- read_yaml(file.path(work_dir, "parameters.yml"))
+  aoi <- sf::read_sf(file.path(work_dir, params$aoi_file))
+
+  from = params$from_date
+  to = params$to_date
+  Output_dir <- params$Output_dir
+
+
+  optram(aoi, from, to, Output_dir, Output_dir)
+  t1 <- Sys.time() 
+  message(t1, "OPTRAM complete") 
+  message("Runtime: ", round(t1-t0), " seconds") ## print runtime duration in seconds
+}
