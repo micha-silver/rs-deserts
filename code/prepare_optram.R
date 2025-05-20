52a21915ce88d36f1144c0e4c31b1214bf0db92f
גיל צמח <tsemachg@auth.ad.bgu.ac.il>
2025-05-20 14:17:22 +0300

    Uploading some initial work made on OPTRAM.

--- code/prepare_optram.R
+++ code/prepare_optram.R
@@ -3,10 +3,59 @@
 # Input: 
 # Returns:
 # Requires:
-# Written By:
+# Written By: Hagar Boneh, Noa Cohen and Gill Tsemach
 
-Prepare_OPTRAM_Model <- function() {
-  
-  t0 <- Sys.time()
-  message(t0, "OPTRAM complete")
-}
+## loading packages
+library(terra)
+library(sf)
+library(sfc)
+library(CDSE)
+library(yaml)
+library(rOPTRAM)
+
+# optram_options()
+# checked that scm_mask is set to TRUE
+
+
+
+work_dir = getwd()
+params <- read_yaml(file.path(work_dir, "parameters.yml"))
+acquire_scihub(save_creds = TRUE, clientid = params$clientid, 
+               secret = params$secret)
+
+optram_options()
+
+getwd()
+work_dir = getwd()
+
+## read parameters
+params <- read_yaml(file.path(work_dir, "parameters.yml"))
+
+aoi <- sf::read_sf(file.path(work_dir, params$aoi_file))
+from = params$from_date
+to = params$to_date
+Output_dir <- params$Output_dir
+
+
+optram(aoi, from, to, Output_dir, Output_dir)
+print("OPTRAM done")
+
+
+# Prepare_OPTRAM_Model <- function() {
+#   
+#   getwd()
+#   work_dir = getwd()
+#   
+#   ## read parameters
+#   params <- read_yaml(file.path(work_dir, "parameters.yml"))
+#   aoi <- sf::read_sf(file.path(work_dir, params$aoi_file))
+#   
+#   from = params$from_date
+#   to = params$to_date
+#   Output_dir <- params$Output_dir
+#   
+# 
+#   optram(aoi, from, to, Output_dir, Output_dir)
+#   t0 <- Sys.time()
+#   message(t0, "OPTRAM complete")
+# }
