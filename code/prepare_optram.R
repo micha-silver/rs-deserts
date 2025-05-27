b8539fe879021e4cd27598bfda887d8c07cc9725
Gill Tsemach <tsemachg@post.bgu.ac.il>
2025-05-27 18:24:25 +0300

    change optram to run without breaking midway

--- code/prepare_optram.R
+++ code/prepare_optram.R
@@ -2,7 +2,7 @@
 # Description: Use rOPTRAM package to prepare soil moisture rasters
 # Input: Parameter.yaml file contains all necessary information
 # Returns:
-# Requires: eOPTRAM, CDSE, yaml, sf
+# Requires: rOPTRAM, CDSE, yaml, sf
 # Written By: Hagar Boneh, Noa Cohen and Gill Tsemach
 
 ## loading packages
@@ -16,7 +16,7 @@ library(rOPTRAM)
 # checked that scm_mask is set to TRUE
 
 ##------------------------- Test section --------------------------
-?optram_safe
+# ?optram_safe
 
 # optram_options()
 # optram_options("overwrite", TRUE, FALSE)
@@ -49,8 +49,7 @@ library(rOPTRAM)
 
 ## ------------------------ Code section -------------------------
 
-## this is to set client ID and secret
-acquire_scihub(save_creds = TRUE, clientid = params$clientid, secret = params$secret)
+
   
 Prepare_OPTRAM_Model <- function() {
 
@@ -67,10 +66,39 @@ Prepare_OPTRAM_Model <- function() {
   from <- params$from_date
   to <- params$to_date
   Output_dir <- params$Output_dir
-
-
-  optram(aoi, from, to, Output_dir, Output_dir)
-  t1 <- Sys.time() 
-  message(t1, "OPTRAM complete") 
-  message("Runtime: ", round(t1-t0), " seconds") ## print runtime duration in seconds
+  
+  ## use tryCatch as temporal solution for using "save_creds = TRUE"
+  #  for saving client ID and secret
+  tryCatch({
+    acquire_scihub(save_creds = TRUE, clientid = params$clientid, secret = params$secret)
+  }, error = function(e) {
+    message(e,"\n")
+  }, finally = {
+    message("downloading...")
+    
+    ## insert MNDWI mask?
+    # aoi_masked <- <mask out water surfaces>
+    # aoi_masked <- aoi[mask_MNDWI]
+    
+    ## or just remove high values?
+    # optram_options("rm.hi.str", TRUE, FALSE) ## STR, affects only the model
+    # # optram_options("rm.low.vi", TRUE, FALSE) ## VI, affects only the model
+    
+    ## for masking each raster, less suitable?
+    # mask(<ras>, <mask>, maskvalues=TRUE) ## terra library
+  
+  
+    ## scm_mask and run optram
+    optram_options("scm_mask", TRUE, show_opts = FALSE) ## just to make sure
+    optram(aoi, from, to, Output_dir, Output_dir) ## run optram wrapper function
+    
+    ## finish
+    t1 <- Sys.time() 
+    message(t1, "OPTRAM complete") 
+    message("Runtime: ", round(difftime(t1,t0, units = "mins"), 2), " mins") ## print runtime duration in seconds
+  })
+  
 }
+
+Prepare_OPTRAM_Model()
+
